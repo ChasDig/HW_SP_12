89285b6db17b72dcaae2051800f328078c65970f
ChasDig <chas.prog@gmail.com>
2022-06-21 21:44:50 +0700

    #3: Create search by meaning.

--- functions.py
+++ functions.py
@@ -1,1 +1,21 @@
 import json
+from typing import List
+
+
+# 3: Load json-file:
+def load_picture() -> list[dict]:
+    with open('posts.json', 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+
+# 3: Create func: search page by meaning:
+def search_page(meaning: str) -> list[dict]:
+
+    result = []
+    for buf in load_picture():
+        if meaning.lower() in buf['content'].lower():
+            result.append(buf)
+
+    return result
+
+
--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, Blueprint, render_template, request
+from functions import search_page
+
 
 # 2: Create Blueprint main_blueprint:
 main_blueprint = Blueprint('main_blueprint', __name__, template_folder='templates')
@@ -8,3 +10,13 @@ main_blueprint = Blueprint('main_blueprint', __name__, template_folder='template
 @main_blueprint.route('/')
 def add_page_main():
     return render_template('index.html')
+
+
+# 3: Create search by meaning:
+@main_blueprint.route('/search')
+def main_search_page():
+
+    search_meaning = request.args['s']
+    result_search_page = search_page(search_meaning)
+
+    return render_template('post_list.html', posts=result_search_page, meaning=search_meaning)
